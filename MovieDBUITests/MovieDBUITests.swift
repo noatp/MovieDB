6cb5a5edf17f787185092356366b033b5d2ab82a
Toan Pham <toan.chpham@gmail.com>
2022-05-24 09:03:05 -0700

    unit and ui testing

--- MovieDBUITests/MovieDBUITests.swift
+++ MovieDBUITests/MovieDBUITests.swift
@@ -22,20 +22,20 @@ class MovieDBUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testInfiniteScroll(){
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        var newCell = app.tables.cells.containing(.staticText, identifier:"Morbius").element
+        XCTAssertTrue(newCell.exists)
+        
+        app.swipeUp()
+        newCell = app.tables.cells.containing(.staticText, identifier:"The Contractor").element
+        XCTAssertTrue(newCell.exists)
+        
+        app.swipeUp()
+        app.swipeUp()
+        newCell = app.tables.cells.containing(.staticText, identifier:"The Grandmother").element
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
